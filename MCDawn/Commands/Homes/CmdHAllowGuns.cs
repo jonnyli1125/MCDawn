47f3ec7dc50aa457c19068da74a8e34bc9897afb
jonnyli1125 <jonny@mcdawn.com>
2012-09-14 19:19:21 -0700

    herp derp, fixed this

--- MCDawn/Commands/Homes/CmdHAllowGuns.cs
+++ MCDawn/Commands/Homes/CmdHAllowGuns.cs
@@ -5,8 +5,8 @@ namespace MCDawn
 {
     public class CmdHAllowGuns : Command
     {
-        public override string name { get { return "hsave"; } }
-        public override string[] aliases { get { return new string[] { "" }; } }
+        public override string name { get { return "hallowguns"; } }
+        public override string[] aliases { get { return new string[] { "hguns" }; } }
         public override string type { get { return "homes"; } }
         public override bool museumUsable { get { return false; } }
         public override LevelPermission defaultRank { get { return LevelPermission.AdvBuilder; } }
