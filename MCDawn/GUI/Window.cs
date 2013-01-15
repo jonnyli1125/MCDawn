b4a894f185624115f873977103d7ed47b391ac77
Jonny <jonny@mcdawn.com>
2013-01-14 18:17:30 -0800

    derp

--- MCDawn/GUI/Window.cs
+++ MCDawn/GUI/Window.cs
@@ -99,7 +99,13 @@ namespace MCDawn.Gui
                 s.OnError += newError;
                 s.OnSystem += newSystem;
 
-                tabControl1.SelectTab(tabControl1.TabPages[0]);
+                new Thread(() => {
+                    Invoke(new Action(() => {
+                        foreach (TabPage tP in tabControl1.TabPages)
+                            tabControl1.SelectTab(tP);
+                        tabControl1.SelectTab(tabControl1.TabPages[0]);
+                    }));
+                }).Start();
 
                 s.HeartBeatFail += HeartBeatFail;
                 s.OnURLChange += UpdateUrl;
